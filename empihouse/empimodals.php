bacf6104f885eb576459b4bf02f4a94d28e32ceb
Laixander Naguit <5080952-laixander@users.noreply.gitlab.com>
2022-05-23 09:25:40 +0800

    20220523 : upload 1

---
+++ empihouse/empimodals.php
@@ -0,0 +1,253 @@
+
+<!-- Portfolio Modals-->
+<!-- Portfolio item 1 modal popup-->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project details-->
+                            <h2 class="text-uppercase">プロジェクト名</h2>
+                            <p class="item-intro text-muted">ザ・ベルベット</p>
+                            <img class="img-fluid shadow border  d-block mx-auto" src="assets/img/portfolio/z1.jpg" alt="..." />
+                            <p>
+                              らきめヒラト目区、差目遊ろいふえユョトへ目阿区巣そんらの課夜尾日うめタコこんゆつ離離野留擢派カメクね根魔れいうここおいいらき手樹絵りろちさとぬはま魔にへゆ。っ名模阿目。つ模派毛氏るけみも他イチユハユ個鵜津素毛。ねよ。
+                            </p>
+                            <ul class="list-inline">
+                                <li>
+                                    <strong>クライアント:</strong>
+                                    Threads
+                                </li>
+                                <li>
+                                    <strong>カテゴリー:</strong>
+                                    Illustration
+                                </li>
+                            </ul>
+                            <table class="table table-bordered">
+                              <thead>
+                                <tr>
+                                  <th>ねよ</th>
+                                  <th class="">っ名模阿目</th>
+                                  <th>めゆえ</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>2018</td>
+                                  <td class="text-start">たら、屋区野根個日雲模擢知みすたノセタサヨオ区都舳おうは、カョノとたえ。</td>
+                                  <td class="text-end">1,000.00</td>
+                                </tr>
+                                  <td>2019</td>
+                                  <td class="text-start">っ名模阿目。つ模派毛氏るけみも他イチユハユ個鵜津素毛。ねよ。</td>
+                                  <td class="text-end">500.00</td>
+                                </tr>
+                                <tr>
+                                  <td>2020</td>
+                                  <td class="text-start">たら、屋区野根個日雲模擢知みすたノセタサヨオ区都舳おうは、カョノとたえ。</td>
+                                  <td class="text-end">1,000.00</td>
+                                </tr>
+                                  <td>2021</td>
+                                  <td class="text-start">ヨッロネア留巣譜氏ナムにアノョミセナネユセッャシオヌア。</td>
+                                  <td class="text-end">700.00</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                <i class="fas fa-xmark me-1"></i>
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio item 2 modal popup-->
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project details-->
+                            <h2 class="text-uppercase">じょせいだんたい</h2>
+                            <p class="item-intro text-muted">水行末 雲来末 風来末。海砂利水魚の。寿限無、寿限無。五劫の擦り切れ、シューリンガンのグーリンダイ。</p>
+                            <img class="img-fluid shadow border  d-block mx-auto" src="assets/img/portfolio/z2.jpg" alt="..." />
+                            <p>シューリンガンのグーリンダイ。長久命の長助。水行末 雲来末 風来末。寿限無、寿限無。パイポパイポ パイポのシューリンガン。やぶら小路の藪柑子、海砂利水魚の、海砂利水魚の、グーリンダイのポンポコピーのポンポコナーの、グーリンダイのポンポコピーのポンポコナーの、やぶら小路の藪柑子、五劫の擦り切れ。長久命の長助、食う寝る処に住む処、寿限無、寿限無、シューリンガンのグーリンダイ、水行末 雲来末 風来末、パイポパイポ パイポのシューリンガン、五劫の擦り切れ。</p>
+                            <ul class="list-inline">
+                                <li>
+                                    <strong>クライアント:</strong>
+                                    Explore
+                                </li>
+                                <li>
+                                    <strong>カテゴリー:</strong>
+                                    Graphic Design
+                                </li>
+                            </ul>
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                <i class="fas fa-xmark me-1"></i>
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio item 3 modal popup-->
+<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project details-->
+                            <h2 class="text-uppercase">ゆうらんけい</h2>
+                            <p class="item-intro text-muted">
+                              シューリンガンのグーリンダイ。水行末 雲来末 風来末。やぶら小路の藪柑子。水行末 雲来末 風来末。
+                            </p>
+                            <img class="img-fluid shadow border  d-block mx-auto" src="assets/img/portfolio/z3.jpg" alt="..." />
+                            <p>やぶら小路の藪柑子、食う寝る処に住む処、シューリンガンのグーリンダイ、グーリンダイのポンポコピーのポンポコナーの。シューリンガンのグーリンダイ、やぶら小路の藪柑子。パイポパイポ パイポのシューリンガン、水行末 雲来末 風来末、海砂利水魚の。海砂利水魚の、パイポパイポ パイポのシューリンガン、水行末 雲来末 風来末。寿限無、寿限無。長久命の長助</p>
+                            <ul class="list-inline">
+                                <li>
+                                    <strong>クライアント:</strong>
+                                    Finish
+                                </li>
+                                <li>
+                                    <strong>カテゴリー:</strong>
+                                    Identity
+                                </li>
+                            </ul>
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                <i class="fas fa-xmark me-1"></i>
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio item 4 modal popup-->
+<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project details-->
+                            <h2 class="text-uppercase">ももクロの作品</h2>
+                            <p class="item-intro text-muted">グーリンダイのポンポコピーのポンポコナーの</p>
+                            <img class="img-fluid shadow border  d-block mx-auto" src="assets/img/portfolio/z4.jpg" alt="..." />
+                            <p>
+                              海砂利水魚の、グーリンダイのポンポコピーのポンポコナーの、長久命の長助。パイポパイポ パイポのシューリンガン、水行末 雲来末 風来末。シューリンガンのグーリンダイ、やぶら小路の藪柑子。グーリンダイのポンポコピーのポンポコナーの。やぶら小路の藪柑子。シューリンガンのグーリンダイ、食う寝る処に住む処、長久命の長助。水行末 雲来末 風来末、海砂利水魚の、食う寝る処に住む処、寿限無、寿限無、五劫の擦り切れ。
+                            </p>
+                            <ul class="list-inline">
+                                <li>
+                                    <strong>クライアント:</strong>
+                                    Lines
+                                </li>
+                                <li>
+                                    <strong>カテゴリー:</strong>
+                                    Branding
+                                </li>
+                            </ul>
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                <i class="fas fa-xmark me-1"></i>
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio item 5 modal popup-->
+<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project details-->
+                            <h2 class="text-uppercase">おねがいします</h2>
+                            <p class="item-intro text-muted">
+                              水行末 雲来末 風来末、食う寝る処に住む処。海砂利水魚の、寿限無、寿限無。
+                            </p>
+                            <img class="img-fluid shadow border  d-block mx-auto" src="assets/img/portfolio/z5.jpg" alt="..." />
+                            <p>
+                                徹はゃぽぞ重佐ヱネナ戒型ぶし横来ヨミメ保派すやへ能教げぞやフ献稲びど治8農ぎの属縮ヨシリ欠個と字加7舎ロ試上ニ移券混いき。大せくち際3受本ホシレル細同聞ま仕基チ週命カハチ記校ユツレ技速めじご倉本が持由ワホノレ話話めトんそ術力ゅ自仁借是噴だな。
+                            </p>
+                            <ul class="list-inline">
+                                <li>
+                                    <strong>クライアント:</strong>
+                                    Southwest
+                                </li>
+                                <li>
+                                    <strong>カテゴリー:</strong>
+                                    Website Design
+                                </li>
+                            </ul>
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                <i class="fas fa-xmark me-1"></i>
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio item 6 modal popup-->
+<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project details-->
+                            <h2 class="text-uppercase">女中</h2>
+                            <p class="item-intro text-muted">参平ッどうを決著う職際四かドそも北見よ芸全江ルたく問現サホヨ済読べイ生注だ点都番ハユホ険敢むり。</p>
+                            <img class="img-fluid shadow border  d-block mx-auto" src="assets/img/portfolio/z6.jpg" alt="..." />
+                            <p>
+                              得たといめ経人ぽもし豊石ハヲチセ整時ネヤワル家要スラき困業ふもはを日混ネケテヌ少震ぎざ弁旧クワ平70国底よ演議ぽ速振さ包芸ネ謎姿税ょぎて。体97任6月正区テヲ辞捜ヘセレ写53協ルンけえ温理オソロ健接ざずきぎ更注くつき要82制ば。働本ゅ概再にちや引正クカ羽限ゅばがぱ子点ス寂二レロ正開スむおぐ田料振ユエ分更ル毎最作ぽトけ隊考ヒタア転済家らへフ実来気メヌシル積変ヌホニリ三帰ぽこせゅ。
+                            </p>
+                            <ul class="list-inline">
+                                <li>
+                                    <strong>クライアント:</strong>
+                                    Window
+                                </li>
+                                <li>
+                                    <strong>カテゴリー:</strong>
+                                    Photography
+                                </li>
+                            </ul>
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                <i class="fas fa-xmark me-1"></i>
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ empihouse/randombg1.php
@@ -0,0 +1,7 @@
+<?php
+
+$array = array("mastheadx1"=>"20", "mastheadx2"=>"42", "mastheadx3"=>"15", "mastheadx4"=>"25");
+print_r(array_rand($array));
+
+
+?>
